f7f873f397f3378e6de93d9c6e1ea352fc242261
gatheuws <Gabriel.THEUWS@etu.uca.fr>
2021-02-23 12:00:02 +0100

    [DEV] test en développement

--- Code/Projet_Barman/app/src/main/java/com/example/projet_barman/MainActivity.java
+++ Code/Projet_Barman/app/src/main/java/com/example/projet_barman/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.projet_barman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import modele.Joueur;
 import modele.Manager;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("D","[DEBUG] version master-dev");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Partie partieActuelle = new Partie(new Niveau(500,1,new Joueur("TEST"),new Shaker(9999)));
