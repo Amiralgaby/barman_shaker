b478c0b0c7a8d46f8c452d08a461a208ef60a019
jeliot <jeliot@iutclinfa1804l.iut.local>
2021-02-25 12:04:13 +0100

    [Dev] Première version des différentes vues du jeu

---
+++ Code/Projet_Barman/app/src/main/java/com/example/projet_barman/JeuActivity.java
@@ -0,0 +1,15 @@
+package com.example.projet_barman;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JeuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
--- Code/Projet_Barman/app/src/main/java/com/example/projet_barman/MainActivity.java
+++ Code/Projet_Barman/app/src/main/java/com/example/projet_barman/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.projet_barman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import modele.Joueur;
+import modele.Manager;
 
 public class MainActivity extends AppCompatActivity {
+    Manager manager=Manager.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void demarrer(View view) {
+        setContentView(R.layout.page_jeu);
+    }
+
+
+    public void jouer(View view) {
+
     }
 }
