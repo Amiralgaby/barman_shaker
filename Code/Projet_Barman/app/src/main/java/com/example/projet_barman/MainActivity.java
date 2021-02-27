fdff15a4f55344064d7ce1e14191e551f01aa347
gatheuws <Gabriel.THEUWS@etu.uca.fr>
2021-02-27 12:36:51 +0100

    [FEATURE] ajout de sensor activity

--- Code/Projet_Barman/app/src/main/java/com/example/projet_barman/MainActivity.java
+++ Code/Projet_Barman/app/src/main/java/com/example/projet_barman/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.projet_barman;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +12,10 @@ import modele.Partie;
 
 public class MainActivity extends AppCompatActivity {
     Manager manager=Manager.getInstance();
-
+    SensorActivity sensorActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Partie partieActuelle = new Partie(FabriqueNiveau.fabriquer(1));
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void demarrer(View view) {
         setContentView(R.layout.page_jeu);
+        sensorActivity = new SensorActivity();
+        Log.d("ON_SENSOR_CHANGED","please do something");
     }
 
 
