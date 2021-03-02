23c4342af0e99ce95a59a05cb1d9c8e1d166ce93
gatheuws <Gabriel.THEUWS@etu.uca.fr>
2021-03-02 12:05:35 +0100

    [DOC] documentation du SensorController

--- Code/Projet_Barman/app/src/main/java/com/example/projet_barman/SensorController.java
+++ Code/Projet_Barman/app/src/main/java/com/example/projet_barman/SensorController.java
@@ -6,10 +6,17 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
+/**
+ * Permet de controller le manager de sensor, et d'obtenir les events du sensor
+ */
 public class SensorController implements SensorEventListener {
     private final SensorManager mSensorManager;
     private final Sensor mAccelerometer;
 
+    /**
+     * Créer une instance de SensorController qui gère les envents et les contrôles de l'accéléromètre et du manager de sensor
+     * @param manager le manager de l'accélèromètre
+     */
     public SensorController(SensorManager manager)
     {
         mSensorManager = manager;
@@ -23,20 +30,35 @@ public class SensorController implements SensorEventListener {
         }
     }
 
+    /**
+     * Ajoute this en tant que listener de l'accélèromètre via le manager de sensor
+     */
     public void register() {
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+    /**
+     * Retire le listener this du manager de sensor
+     */
     public void unregister() {
         mSensorManager.unregisterListener(this);
     }
 
+    /**
+     * Appelée lorsque qu'il y a un nouvel envent
+     * @param event le sensor event
+     */
     @Override
     public void onSensorChanged(SensorEvent event) {
         // pour obtenir la valeur faire event.values[1]
-        Log.d("CALCUL", "la valeur y : " + event.values[1]);
+        Log.d("DONNEES", "la valeur y : " + event.values[1]);
     }
 
+    /**
+     * Appelée quand la précision du sensor change
+     * @param sensor le sensor sur lequel est affecté le changement de précision
+     * @param accuracy la nouvelle valeur de la précision
+     */
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // appelé lorsque l'accuracy est changée
