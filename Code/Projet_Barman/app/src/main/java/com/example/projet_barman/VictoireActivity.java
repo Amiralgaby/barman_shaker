bada3092e7cf4469ecc7a0aa5a54779ec4e40f82
gatheuws <Gabriel.THEUWS@etu.uca.fr>
2021-03-15 14:08:39 +0100

    [DEV] developpement pour victoire

---
+++ Code/Projet_Barman/app/src/main/java/com/example/projet_barman/VictoireActivity.java
@@ -0,0 +1,12 @@
+package com.example.projet_barman;
+
+import android.app.Activity;
+import android.view.View;
+
+public class VictoireActivity extends Activity {
+    public void Quitter(View view) {
+    }
+
+    public void niveauSuivant(View view) {
+    }
+}
